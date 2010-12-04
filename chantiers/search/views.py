7cfdd5946205ea6582a654fe8ee17b517a29e28b
Michael Mulley <michael@michaelmulley.com>
2010-12-04 16:54:04 -0500

    add JSONP

--- chantiers/search/views.py
+++ chantiers/search/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from django.contrib.gis.geos import LineString
 from django.http import HttpResponse, Http404
@@ -7,20 +8,19 @@ from django.template import loader, RequestContext
 
 from chantiers.points.models import Chantier
 
-
 def points_via_line(request):
     
-    if request.POST and request.is_ajax():
-        raw_line = request.POST.get('line', None)
-        print 'hi'
-        line = LineString(json.loads(raw_line))
-        chantiers = Chantier.objects.get_near_line(line)
-        print 'there'
-        t = loader.get_template("point_list.html")
-        c = RequestContext(request, {'chantiers': chantiers})
-        return HttpResponse(t.render(c), mimetype="application/javascript")
-        
-    raise Http404
+    raw_line = request.REQUEST.get('line', None)
+    if not line:
+        raise Http404
+    line = LineString(json.loads(raw_line))
+    chantiers = Chantier.objects.get_near_line(line)
+    t = loader.get_template("point_list.html")
+    c = RequestContext(request, {'chantiers': chantiers})
+    resp = t.render(c)
+    if 'callback' in request.GET:
+        resp = "%s(%s);" % (re.sub(r'[^a-zA-Z0-9_]', '', request.GET['callback']), resp)
+    return HttpResponse(resp, mimetype="application/javascript")
     
 def prototype(request):
     return render_to_response('prototype.html', {}, RequestContext(request))
